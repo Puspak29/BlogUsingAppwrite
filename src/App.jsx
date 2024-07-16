1bdfbb1b39ebb6fea300c86ca9aa31301f81a089
Puspak29 <samantapuspak@gmail.com>
2024-07-16 23:59:29 +0530

    added tailwind and provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authServiece from './Appwrite/auth'
+import { logout } from './Store/AuthSlice'
+import { Header, Footer } from './components'
 
 function App() {
+  const [loading, setLoading]= useState(true)
+  const dispatch= useDispatch()
 
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  )
+  useEffect(()=>{
+    authServiece.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    }).finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div>
+      <div>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
